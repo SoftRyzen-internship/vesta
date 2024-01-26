d12730d5b248cbd4ca094afa4ad58611fc58a4ea
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-01-26 13:42:25 +0200

    delete common index.ts in sectionss folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { TestClientComponent } from "@/components/base/TestClientComponent/TestClientComponent"; // ! -- remove later
-import { Hero } from "@/sections";
+import { Hero } from "@/sections/Hero";
 
 export default async function Home() {
   return (
